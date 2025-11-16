694a54da18a67734de259cf8083c0747124ad037
SeonWu Kim <sunwoo005@naver.com>
2025-11-16 19:08:58 +0900

    feat: 판매금액, 등수별 비율, 이월, 당첨자수 계산 구현

--- internal/lotto/round.go
+++ internal/lotto/round.go
@@ -36,6 +36,52 @@ type RoundOutput struct {
 	PoolAfterCap map[Rank]int // 상한 적용 후 풀 금액
 	PaidPerWin   map[Rank]int // 등수별 1인당 지급액
 	PaidTotal    map[Rank]int // 등수별 총 지급액
-	CarrayOut    map[Rank]int // 등수별 다음 회차로 이월되는 금액
+	CarryOut     map[Rank]int // 등수별 다음 회차로 이월되는 금액
 	RollDown     map[Rank]int // 상한 초과로 하위 등수로 내려보낸 금액
+}
+
+func CalculateRound(in RoundInput) RoundOutput {
+	out := newRoundOutput(in)
+
+	if in.Mode != ModeParimutuel {
+		return out
+	}
+
+	for _, a := range in.Allocations {
+		calcRankPool(in, &out, a)
+	}
+
+	return out
+}
+
+func newRoundOutput(in RoundInput) RoundOutput {
+	return RoundOutput{
+		Sales:        in.Sales,
+		PoolBefore:   make(map[Rank]int),
+		PoolAfterCap: make(map[Rank]int),
+		PaidPerWin:   make(map[Rank]int),
+		PaidTotal:    make(map[Rank]int),
+		CarryOut:     make(map[Rank]int),
+		RollDown:     make(map[Rank]int),
+	}
+}
+
+func calcRankPool(in RoundInput, out *RoundOutput, a Allocation) {
+	basePool := in.Sales * a.BasisPoints / BasisPoints
+	carry := in.CarryIn[a.Rank]
+	pool := basePool + carry
+
+	out.PoolBefore[a.Rank] = pool
+	out.PoolAfterCap[a.Rank] = pool
+
+	winners := in.Winners[a.Rank]
+	if winners <= 0 {
+		// 당첨자가 없으면 전액 이월
+		out.CarryOut[a.Rank] = pool
+		return
+	}
+	// 당첨자 있으면 균등 분배
+	perWinner := pool / winners
+	out.PaidPerWin[a.Rank] = perWinner
+	out.PaidTotal[a.Rank] = perWinner * winners
 }
